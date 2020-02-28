08ea5882b18567314470d284dbf5b12db2c2b11b
BuildTools <unconfigured@null.spigotmc.org>
2020-02-28 13:51:41 +0000

    Basics

---
+++ src/main/java/tk/tropicaldan/energydrinks/utils/EnergyDrinkInfo.java
@@ -0,0 +1,9 @@
+package tk.tropicaldan.energydrinks.utils;
+
+public class EnergyDrinkInfo {
+    public static final String MODID = "energydrinks";
+    public static final String MODDISPLAYNAME = "EnergyDrinks";
+    public static final String VERSION = "1.15.2";
+
+
+}
